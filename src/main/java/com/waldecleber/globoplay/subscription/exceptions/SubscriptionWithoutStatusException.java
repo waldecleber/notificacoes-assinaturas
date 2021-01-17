7d2ed55be6b79d48050491feefffdbe40374fa7b
Waldecleber Oliveira Gonçalves <waldecleber.g@e-unicred.com.br>
2021-01-17 19:48:32 -0300

    Adiciona Exception

---
+++ src/main/java/com/waldecleber/globoplay/subscription/exceptions/SubscriptionWithoutStatusException.java
@@ -0,0 +1,12 @@
+package com.waldecleber.globoplay.subscription.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+public class SubscriptionWithoutStatusException extends RuntimeException {
+
+    public SubscriptionWithoutStatusException(String msg) {
+        super(msg);
+    }
+}
