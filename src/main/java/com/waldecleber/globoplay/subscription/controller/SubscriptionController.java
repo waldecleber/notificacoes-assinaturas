df634dd57ba66ba9c419da21979ba0e619321fae
Waldecleber Oliveira Gonçalves <waldecleber.g@e-unicred.com.br>
2021-01-17 19:51:54 -0300

    Cria um controller para enviar dados para a fila.

---
+++ src/main/java/com/waldecleber/globoplay/subscription/controller/SubscriptionController.java
@@ -0,0 +1,32 @@
+package com.waldecleber.globoplay.subscription.controller;
+
+import com.waldecleber.globoplay.subscription.dto.SubscriptionDTO;
+import com.waldecleber.globoplay.subscription.service.SubscriptionService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/subscriptions")
+public class SubscriptionController {
+
+    @Autowired
+    private SubscriptionService subscriptionService;
+
+    @ApiOperation(value = "Save a subscription")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Subscription saved"),
+            @ApiResponse(code = 403, message = "You don't have permission to access this resource"),
+            @ApiResponse(code = 500, message = "An exception was thrown"),
+    })
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void save(@RequestBody SubscriptionDTO subscription) {
+        subscriptionService.sendSubscriptionToRabbit(subscription);
+    }
+
+}
