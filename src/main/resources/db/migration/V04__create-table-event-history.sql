445dfb9b2aa36db440bb7308755ad81cb80c8efb
Waldecleber Oliveira Gonçalves <waldecleber.g@e-unicred.com.br>
2021-01-19 12:22:40 -0300

    Removido NOT NULL indevido do script SQL

--- src/main/resources/db/migration/V04__create-table-event-history.sql
+++ src/main/resources/db/migration/V04__create-table-event-history.sql
@@ -1,7 +1,7 @@
 CREATE TABLE event_history (
   id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
   type varchar(20) NOT NULL,
-  subscription_id varchar(255) NOT NULL,
+  subscription_id varchar(255),
   created_at timestamp DEFAULT CURRENT_TIMESTAMP
 );
 
